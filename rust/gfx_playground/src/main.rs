96efd9e5fea852fabd7f5f1bb9bfeeec7ba70b69
Ollie W <owright@tuta.io>
2019-12-15 03:30:13 +0000

    14/12/19 gfx_playground edits

--- rust/gfx_playground/src/main.rs
+++ rust/gfx_playground/src/main.rs
@@ -1,8 +1,10 @@
 mod pong;
+mod systems;
+
 use crate::pong::Pong;
-
-
 use amethyst::{
+    core::TransformBundle,
+    input::{InputBundle, StringBindings},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -12,38 +14,45 @@ use amethyst::{
     utils::application_root_dir,
 };
 
-
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
-    let app_root = application_root_dir()?;
-    let display_config_path = app_root.join("config").join("display.ron");
-    
 
-    let mut world = World::new();
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config/display.ron");
+
+    // This line is not mentioned in the pong tutorial as it is specific to the context
+    // of the git repository. It only is a different location to load the assets from.
+    let assets_dir = app_root.join("assets");
+
     let game_data = GameDataBuilder::default()
+        // Add the transform bundle which handles tracking entity positions
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            InputBundle::<StringBindings>::new().with_bindings_from_file(
+                app_root.join("config/bindings.ron"),
+            )?,
+        )?
+        // We have now added our own systems, defined in the systems module
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(
+            systems::BounceSystem,
+            "collision_system",
+            &["paddle_system", "ball_system"],
+        )
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
-            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
-            .with_plugin(
-                
-                RenderToWindow::from_config_path(display_config_path)
-                    .with_clear([0.00196, 0.23726, 0.21765, 1.0]),
-            )
-            
-            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-            .with_plugin(RenderFlat2D::default()),
+                // The RenderToWindow plugin provides all the scaffolding for opening a window and
+                // drawing on it
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                // RenderFlat2D plugin is used to render entities with `SpriteRender` component.
+                .with_plugin(RenderFlat2D::default()),
         )?;
 
-
-    let assets_dir = app_root.join("assets");
-    let mut game = Application::new(assets_dir, Pong, game_data)?;
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
     game.run();
-    
-
     Ok(())
 }
-
-
-
-
-// You are at the "initialise some entities" stage in the Pong tutorial for the rust amethyst book
