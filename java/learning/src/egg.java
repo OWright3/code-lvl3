1bf85f95e645ba4e59e2e30312e44a189e110432
Oliver Wright <44814748+OWright3@users.noreply.github.com>
2020-04-27 16:51:42 +0100

    Late April Update
    more focus placed on subjects like maths lately

---
+++ java/learning/src/egg.java
@@ -0,0 +1,19 @@
+// comments are cool
+/*
+multiline comments are also very cool
+as shown here.
+*/
+
+/************************************************
+ This is documentation. There only needs to be 2 astrix at the beginning bracket and 1 at the end,
+ all other astrix are extra.
+ ************************************************/
+
+public class egg{
+    public static void main(String[] args) {
+        String chicken = "retard";
+
+
+        System.out.println(chicken);
+    }
+}
